ac4e2b4efed6fa6e380959b8aa5ec535f82445a8
Indrasen Singh <insin@microsoft.com>
2022-04-10 02:45:44 +0530

    Changes to handle duplicate character in the input

--- worlde.py
+++ worlde.py
@@ -17,7 +17,9 @@ for i in range(5):
     listOfLists.append(a_z)
 
 # All 5 letters will be in this regex
-regexexp = openbracket + listOfLists[0] + closebracket + openbracket + listOfLists[1] + closebracket + openbracket + listOfLists[2] + closebracket + openbracket + listOfLists[3] + closebracket + openbracket + listOfLists[4] + closebracket + dollar
+regexexp = openbracket + listOfLists[0] + closebracket + openbracket + listOfLists[1] + closebracket + openbracket + \
+    listOfLists[2] + closebracket + openbracket + listOfLists[3] + \
+    closebracket + openbracket + listOfLists[4] + closebracket + dollar
 r = re.compile(regexexp)
 possibleSolution = list(filter(r.match, possibleSolution))
 print(possibleSolution)
@@ -31,26 +33,35 @@ while len(possibleSolution) > 1:
             break
         else:
             print("Invalid input, try again")
-        
+
     for i in range(0, len(val)):
-        pos = int(input( 'is "' + val[i] + '" green(1), yellow(2), black(3): '))
-        if pos == 1: # The character exists in the answer at the correct position
+        pos = int(input('is "' + val[i] + '" green(1), yellow(2), black(3): '))
+        if pos == 1:  # The character exists in the answer at the correct position
             listOfLists[i] = val[i]
             answerCharSet.add(val[i])
-        elif pos == 2: # The character exists in the answer at the wrong position
+        elif pos == 2:  # The character exists in the answer at the wrong position
             listOfLists[i] = listOfLists[i].replace(val[i], "")
             answerCharSet.add(val[i])
-        elif pos == 3: # The character does not exist in the answer
-            for j in range(0, len(listOfLists)):
-                listOfLists[j] = listOfLists[j].replace(val[i], "")
+        elif pos == 3:  # The character does not exist in the answer
+            flag = True
+            for j in range(0, len(val)): # Check if the same character is entered before, in that case it is NO OP
+                if val[i] == val[j] and i != j:
+                    flag = False
+                    break
+
+            if flag:
+                for j in range(0, len(listOfLists)):
+                    listOfLists[j] = listOfLists[j].replace(val[i], "")
 
     # Update the regex and filter the possible solutions
-    regexexp = openbracket + listOfLists[0] + closebracket + openbracket + listOfLists[1] + closebracket + openbracket + listOfLists[2] + closebracket + openbracket + listOfLists[3] + closebracket + openbracket + listOfLists[4] + closebracket + dollar
+    regexexp = openbracket + listOfLists[0] + closebracket + openbracket + listOfLists[1] + closebracket + openbracket + \
+        listOfLists[2] + closebracket + openbracket + listOfLists[3] + \
+        closebracket + openbracket + listOfLists[4] + closebracket + dollar
     r = re.compile(regexexp)
     possibleSolution = list(filter(r.match, possibleSolution))
 
     # Iterate through possibleSolution and check if the word is in answerCharSet
-    if answerCharSet :
+    if answerCharSet:
         for letter in answerCharSet:
             regexexp = a_zAstricks + letter + a_zAstricks
             r = re.compile(regexexp)
